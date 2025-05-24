e2390da40369e89c315523c44b77ec36d365d03c
Aladdin-2 <aladdin19.11.21@gmail.com>
2025-05-24 22:13:41 +0400

    Payment project created

---
+++ src/test/java/az/aladdin/lesson6/Lesson6ApplicationTests.java
@@ -0,0 +1,13 @@
+package az.aladdin.lesson6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
